8036c50523503cda872c06c17769d66710f4a9e8
RodrigoMesel <rmlo@cin.ufpe.br>
2021-08-16 16:37:13 -0300

    a

--- movimentos.py
+++ movimentos.py
@@ -6,22 +6,31 @@ class Pikachu(pg.sprite.Sprite):
         self.width = width
         self.height = height
         self.img = pg.transform.scale(pg.image.load("Assets/pikachuskate.png"), (self.width, self.height))
-        self.velocidadeX = 0.17
+        self.velocidadeX = 0.27
         self.windowWidth = windowWidth
         self.windowHeight = windowHeight
         self.x = windowWidth/2 - width/2
         self.y = 600
         self.tela = tela
+        self.direcao = -1
     
     def draw(self):
         self.tela.blit(self.img, (self.x, self.y))
 
+
     def move(self):
         comandos = pg.key.get_pressed()
-        if comandos[pg.K_RIGHT]:
+        if comandos[pg.K_RIGHT] and not(self.x >= self.windowWidth-self.width):
             self.x += self.velocidadeX
-            self.img = pg.transform.flip(self.img, True, False)
-        if comandos[pg.K_LEFT]:
-            self.x += -self.velocidadeX
-            self.img = pg.transform.flip(self.img, True, False)
+            if self.direcao == -1:
+                self.img = pg.transform.flip(self.img, True, False)
+            self.direcao = 1
+        if comandos[pg.K_LEFT] and not(self.x <= 0):
+            self.x -= self.velocidadeX
+            if self.direcao == 1:
+                self.img = pg.transform.flip(self.img, True, False)
+            self.direcao = -1
+
+
+
 
