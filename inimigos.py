7aebbdc5fde35bcc20045f9f0dd7afc7e0e602c7
unknown <rmlo@cin.ufpe.br>
2021-08-16 10:18:15 -0300

    troquei pygame por pg

--- inimigos.py
+++ inimigos.py
@@ -1,19 +1,19 @@
-import pygame
+import pygame as pg
 from sys import exit
 from random import randint
 
 
-pygame.init()
+pg.init()
 
-class Farfetch(pygame.sprite.Sprite):
+class Farfetch(pg.sprite.Sprite):
   # Classe responsável por criar o inimigo farfetch
   def __init__(self, width, height, windowWidth, windowHeight, tela):
     angulo = 25
     self.width = width
     self.height = height
-    self.img = pygame.transform.scale(pygame.image.load('assets/farfetch.png'), (self.width, self.height))
-    self.img = pygame.transform.rotate(self.img, angulo)
-    self.img = pygame.transform.flip(self.img, True, False)
+    self.img = pg.transform.scale(pg.image.load('assets/farfetch.png'), (self.width, self.height))
+    self.img = pg.transform.rotate(self.img, angulo)
+    self.img = pg.transform.flip(self.img, True, False)
     self.velocidadeY = 0.32
     self.velocidadeX = 0.29
     self.windowWidth = windowWidth
@@ -34,30 +34,30 @@ class Farfetch(pygame.sprite.Sprite):
       self.y = -self.height
       self.x = randint(0, self.windowWidth-self.width)
     if self.x >= self.windowWidth-self.width*4/5:
-      self.img = pygame.transform.flip(self.img, True, False)
+      self.img = pg.transform.flip(self.img, True, False)
       self.velocidadeX = -self.velocidadeX
     if self.x <= -self.width/5:
-      self.img = pygame.transform.flip(self.img, True, False)
+      self.img = pg.transform.flip(self.img, True, False)
       self.velocidadeX = -self.velocidadeX
 
 
 largura, altura = 600, 720
 
 
-tela = pygame.display.set_mode((largura,altura))
-pygame.display.set_caption('Jorginho')
+tela = pg.display.set_mode((largura,altura))
+pg.display.set_caption('Jorginho')
 
 
 farfetch = Farfetch(80, 80, largura, altura, tela)
-background = pygame.image.load("Assets/background.jpeg")
+background = pg.image.load("Assets/background.jpeg")
 while True:
   tela.fill((0,0,0))
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      pygame.quit()
+  for event in pg.event.get():
+    if event.type == pg.QUIT:
+      pg.quit()
       exit()
   tela.blit(background,(0,0))
   farfetch.draw()
   farfetch.update(farfetch.x, farfetch.y)
 
-  pygame.display.update()
+  pg.display.update()
